06fff4970a40678dc141daf89a339f7b891ee101
许冬冬 <awsd313025@163.com>
2024-10-11 13:27:27 +0800

    修复添加一个子项会添加到所有子项

--- src/components/任务树/tools/index.ts
+++ src/components/任务树/tools/index.ts
@@ -5,6 +5,7 @@ export function convertTo树(数据: I事项[]): I事项[] {
   const itemMap: I事项[] = [];
 
   数据.forEach((事项) => {
+    事项.子项 = [];
     itemMap[事项.id] = 事项;
   });
 
@@ -12,9 +13,7 @@ export function convertTo树(数据: I事项[]): I事项[] {
 
   数据.forEach((事项) => {
     if (!事项.父项.includes(顶级节点)) {
-      if (itemMap[事项.父项]) {
-        itemMap[事项.父项].子项.push(itemMap[事项.id]);
-      }
+      itemMap[事项.父项].子项.push(itemMap[事项.id]);
     } else {
       树.push(itemMap[事项.id]);
     }
--- src/constant/初始值.ts
+++ src/constant/初始值.ts
@@ -5,17 +5,6 @@ import dayjs from "dayjs";
 import { 事项状态, 已完成, 进行中, 重复中, 顶级节点 } from "./状态配置";
 import { nanoid } from "nanoid";
 
-const 顶级节点公共属性 = {
-  重要程度: NaN,
-  紧急程度: NaN,
-  开始时间: dayjs(NaN),
-  结束时间: dayjs(NaN),
-  重复: undefined,
-  checkable: false,
-  子项: [],
-  层级: 0 as T层级,
-};
-
 export const 任务树初始值: ({
   id: string;
   key: string;
@@ -28,7 +17,6 @@ export const 任务树初始值: ({
     名称: "重复中",
     状态: 事项状态.重复中,
     父项: stringArr2string([重复中, 顶级节点]),
-    ...顶级节点公共属性,
   },
   {
     id: nanoid(),
@@ -36,7 +24,6 @@ export const 任务树初始值: ({
     名称: "进行中",
     状态: 事项状态.进行中,
     父项: stringArr2string([进行中, 顶级节点]),
-    ...顶级节点公共属性,
   },
   {
     id: nanoid(),
@@ -44,8 +31,17 @@ export const 任务树初始值: ({
     名称: "已完成",
     状态: 事项状态.已完成,
     父项: stringArr2string([已完成, 顶级节点]),
-    ...顶级节点公共属性,
   },
-];
+].map((item) => ({
+  ...item,
+  重要程度: NaN,
+  紧急程度: NaN,
+  开始时间: dayjs(NaN),
+  结束时间: dayjs(NaN),
+  重复: undefined,
+  checkable: false,
+  子项: [],
+  层级: 0 as T层级,
+}));
 
 export const 各层级数量初始值 = [3, 2, 0, 0, 0, 0];
