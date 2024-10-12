c02fe9600eda14863f8d86bccd87ad75f6666070
许冬冬 <awsd313025@163.com>
2024-10-12 18:43:39 +0800

    添加领域

--- src/pages/主页/index.tsx
+++ src/pages/主页/index.tsx
@@ -1,5 +1,5 @@
 import { E数据索引 } from "@/constant/系统码";
-import 事项树 from "@/pages/主页/components/事项树";
+import 领域 from "./components/领域";
 
 export interface I主页Props {
   加载数据: (key: E数据索引) => Promise<any>;
@@ -7,7 +7,7 @@ export interface I主页Props {
 }
 
 function Home() {
-  return <事项树 />;
+  return <领域 />;
 }
 
 export default Home;
