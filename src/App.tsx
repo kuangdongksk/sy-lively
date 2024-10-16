d8c12110a19cb798c14f3660b783c983e33792aa
许冬冬 <awsd313025@163.com>
2024-10-16 08:55:51 +0800

    修复：函数前加async导致运行出错

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { 用户设置Atom } from "./jotai/用户设置";
 
 const { Header, Footer, Sider, Content } = Layout;
 
-async function App() {
+function App() {
   const 当前位置 = useLocation();
   const 导航到 = useNavigate();
   const [, 设置用户设置] = useAtom(用户设置Atom);
