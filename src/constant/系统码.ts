b03b21f9b12591482e32d9fbcb22f76c97d9ccfb
许冬冬 <awsd313025@163.com>
2024-10-17 15:05:53 +0800

    功能：添加日记跳转

--- src/constant/系统码.ts
+++ src/constant/系统码.ts
@@ -1,4 +1,6 @@
 export const 自定义 = "custom";
+export const 思源协议 = "siyuan://blocks/";
+
 export const 喧嚣 = "lively";
 export const 领域 = "domain";
 export const 属性前缀 = `${自定义}-plugin-${喧嚣}-`;
@@ -7,7 +9,7 @@ export enum E块属性名称 {
   用户设置 = `${属性前缀}userSettings`,
   领域设置 = `${属性前缀}${领域}`,
   事项 = `${属性前缀}things`,
-  日记前缀 = `${自定义}-dailynote-`
+  日记前缀 = `${自定义}-dailynote-`,
 }
 
 export enum E数据索引 {
--- src/pages/日历/index.tsx
+++ src/pages/日历/index.tsx
@@ -1,7 +1,36 @@
-import { Calendar } from "antd";
+import { 获取笔记本下的对应日期的日记文档 } from "@/API/文档/获取";
+import { 思源协议 } from "@/constant/系统码";
+import { 用户设置Atom } from "@/store/用户设置";
+import { JavaOutlined } from "@ant-design/icons";
+import { Button, Calendar, Tooltip } from "antd";
+import { useAtom } from "jotai";
 
 function 日历() {
-  return <Calendar />;
+  const [用户设置] = useAtom(用户设置Atom);
+  return (
+    <Calendar
+      cellRender={(value, info) => {
+        return (
+          <div>
+            <Tooltip title="跳转到日记">
+              <Button
+                type="link"
+                icon={<JavaOutlined />}
+                onClick={() => {
+                  获取笔记本下的对应日期的日记文档(
+                    用户设置.笔记本ID,
+                    value
+                  ).then(({ id }) => {
+                    window.open(思源协议 + id);
+                  });
+                }}
+              />
+            </Tooltip>
+          </div>
+        );
+      }}
+    />
+  );
 }
 
 export default 日历;
