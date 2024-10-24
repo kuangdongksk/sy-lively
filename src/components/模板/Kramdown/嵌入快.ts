c2898ee5bf0d6d5b3ac292121611d8c0425d05ce
许冬冬 <awsd313025@163.com>
2024-10-24 16:27:29 +0800

    修复：嵌入块ID与记录不符

--- src/components/模板/Kramdown/嵌入快.ts
+++ src/components/模板/Kramdown/嵌入快.ts
@@ -1,12 +1,11 @@
 import { E时间格式化 } from "@/constant/配置常量";
-import { I事项 } from "@/pages/主页/components/事项树/components/事项";
+import {} from "@/pages/主页/components/事项树/components/事项";
+import { I事项 } from "@/types/喧嚣";
 import { 生成块ID } from "@/utils/DOM";
 import dayjs from "dayjs";
 
 export function 生成嵌入块Kramdown(事项: I事项) {
-  return `{{select * from blocks where id='${
-    事项.ID
-  }'}}\n{: id="${生成块ID()}" updated="${dayjs().format(
-    E时间格式化.思源时间
-  )}"}`;
+  return `{{select * from blocks where id='${事项.ID}'}}\n{: id="${
+    事项.嵌入块ID
+  }" updated="${dayjs().format(E时间格式化.思源时间)}"}`;
 }
