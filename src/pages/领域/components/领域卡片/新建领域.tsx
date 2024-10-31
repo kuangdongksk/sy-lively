6f34aba785398e31f11d49e1273a24872f1fbf55
许冬冬 <awsd313025@163.com>
2024-10-31 20:49:13 +0800

    重构：将三个表单统一为增改查表单

--- src/pages/领域/components/领域卡片/新建领域.tsx
+++ src/pages/领域/components/领域卡片/新建领域.tsx
@@ -1,15 +1,18 @@
+import { I增改查弹窗表单Ref } from "@/components/增改查弹窗表单";
 import 领域表单 from "@/业务组件/表单/领域表单";
+import { useRef } from "react";
 import { useStyle } from "./index.styles";
-import { T弹窗状态 } from "@/components/弹窗表单";
-import { useState } from "react";
 
 function 新建领域() {
   const { styles } = useStyle();
 
-  const [弹窗状态, 令弹窗状态为] = useState<T弹窗状态>(undefined);
+  const 领域表单Ref = useRef<I增改查弹窗表单Ref>(null);
 
   return (
-    <div className={styles.卡片} onClick={() => 令弹窗状态为("添加")}>
+    <div
+      className={styles.卡片}
+      onClick={() => 领域表单Ref.current?.令表单状态为("添加")}
+    >
       <div className={styles.卡片标题}>
         <div className={styles.卡片标题第一行}>
           <span>新建领域</span>
@@ -20,7 +23,7 @@ function 新建领域() {
         <div className={styles.卡片内容中间}></div>
       </div>
 
-      <领域表单 弹窗状态={弹窗状态} 令弹窗状态为={令弹窗状态为} />
+      <领域表单 ref={领域表单Ref} />
     </div>
   );
 }
