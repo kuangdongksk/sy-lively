4ab55dd3cb934fdbf6258db0c7c1e509a36089d5
许冬冬 <awsd313025@163.com>
2026-01-27 15:48:07 +0800

    优化：将自己实现的消息推送转化为siyuan实现

--- src/tools/卡片.ts
+++ src/tools/卡片.ts
@@ -1,14 +1,14 @@
-import MsgSender from "@/class/helper/MsgSender";
+import { showMessage } from "siyuan";
 import SY文档 from "@/class/思源/文档";
 
 export async function 校验卡片文档是否存在(ID: string) {
   if (!ID) {
-    MsgSender.sySendError({ msg: "请先在设置中生成卡片文档", timeout: 5000 });
+    showMessage("请先在设置中生成卡片文档", 5000, "error");
     return false;
   }
 
   if (!(await SY文档.检验文档是否存在(ID))) {
-    MsgSender.sySendError({ msg: "卡片文档不存在，请重新生成", timeout: 5000 });
+    showMessage("卡片文档不存在，请重新生成", 5000, "error");
     return false;
   }
   return true;
